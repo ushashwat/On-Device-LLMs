3a8821020d6b1c48313766ebe1730d7a94b677f9
Shashwat <work.shashwat@gmail.com>
2025-10-31 18:08:04 +0000

    Readjust code, modify function to return dict, use .map() instead of .apply()

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import torch
 import pandas as pd
+from datasets import Dataset
 from peft import PeftModel
 from transformers import (
     AutoTokenizer,
@@ -21,7 +22,9 @@ SYS_PROMPT = """
 ### Constraints: If the question is not related to vehicle diagnostics, politely refuse to answer.
 """
 
+# Read test set and convert to Dataset object
 df_test = pd.read_json(test_file, lines=True)
+data_test = Dataset.from_pandas(df_test)
 
 # 4-bit quantisation config
 bnb_config = BitsAndBytesConfig(
@@ -34,38 +37,20 @@ bnb_config = BitsAndBytesConfig(
 # Load model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(
     model_id,
+    trust_remote_code=True,
     quantization_config=bnb_config,
     dtype=torch.bfloat16,
 )
 
-tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side='left')
+tokenizer = AutoTokenizer.from_pretrained(
+    model_path,
+    trust_remote_code=True,
+    padding_side="left"
+)
 
 # Load trained adapters and set to eval mode
 model = PeftModel.from_pretrained(model, model_path)
 model.eval()
-
-# Build the prompt
-def apply_chat_template(example, tokenizer):
-    """
-    Takes each example from the test set and applies the chat template.
-    """
-    messages = [
-        {"role": "system", "content": SYS_PROMPT},
-        {"role": "user", "content": example["question"]}
-    ]
-
-    prompt_text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True,
-    )
-    return prompt_text
-
-# Apply chat template to test set
-prompts = df_test.apply(
-    lambda ex: apply_chat_template(ex, tokenizer),
-    axis=1
-).to_list()
 
 generation_kwargs = {
     "max_new_tokens": 512,
@@ -73,21 +58,38 @@ generation_kwargs = {
     "return_full_text": False,
 }
 
-# Create pipeline and run evaluation
+# Build the prompt
+def apply_chat_template(example):
+    """
+    Applies the chat template to a batch of samples from the test set.
+    """
+    messages = [
+        {"role": "system", "content": SYS_PROMPT},
+        {"role": "user", "content": example["question"]}
+    ]
+    prompt = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+    )
+
+    return {"prompt": prompt}
+
+# Apply chat template & get the list of prompts
+data_prompts = data_test.map(apply_chat_template)
+prompts_list = data_prompts["prompt"][:]
+
+# Create pipeline and get predictions
 pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-outputs = pipe(prompts, **generation_kwargs)
+outputs = pipe(prompts_list, **generation_kwargs, batch_size=8)
+pred = [output[0]["generated_text"].strip() for output in outputs]
 
-
-
-# Get predictions
-pred = [output[0]['generated_text'].strip() for output in outputs]
-results = pd.DataFrame({
-    "question": df_test["question"],
-    "answer_ref": df_test["answer"],
+# Save evaluation results
+df_results = pd.DataFrame({
+    "question": data_test["question"],
+    "answer_ref": data_test["answer"],
     "answer_pred": pred
 })
 
 print("Evaluation complete.")
-
-# Save evaluation results
-results.to_csv(results_path, index=False)
+df_results.to_csv(results_path, index=False)
